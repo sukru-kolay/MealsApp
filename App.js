304facd31035e87b4c1a6992a247a18649c4c2fc
sukru kolay <sukruextraegitim@outlook.com>
2023-05-30 10:22:27 +0300

    bottom tab navigator added

--- App.js
+++ App.js
@@ -5,41 +5,71 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoriteScreen from "./screens/FavoriteScreen";
+import { Ionicons } from "react-native-vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+const MealsStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351301" },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: "#3f2f25" },
+      }}
+    >
+      <Stack.Screen
+        name="MealsCategories"
+        component={CategoriesScreen}
+        options={{
+          title: "All Categories",
+          headerTitleAlign: "center",
+        }}
+      />
+      <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+      <Stack.Screen name="MealDetailScreen" component={MealDetailScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const AppBottomTab = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Meal") {
+            iconName = focused ? "restaurant" : "restaurant-outline";
+          } else if (route.name === "Favorite") {
+            iconName = focused ? "heart" : "heart-outline";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        headerShown: false,
+        tabBarStyle: { backgroundColor: "#351301" },
+      })}
+      tabBarOptions={{
+        activeTintColor: "#A36B4D",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <Tab.Screen name="Meal" component={MealsStack} />
+      <Tab.Screen name="Favorite" component={FavoriteScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
       <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: "#351301" },
-            headerTintColor: "white",
-            contentStyle: { backgroundColor: "#3f2f25" },
-          }}
-        >
-          <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
-            options={{
-              title: "All Categories",
-              headerTitleAlign: "center",
-            }}
-          />
-          <Stack.Screen
-            name="MealsOverview"
-            component={MealsOverviewScreen}
-            // options={
-            //   // title: "All Categories",
-            //   // headerTitleAlign: "center",
-            //   ({ route, navigation }) => {
-            //     const catId = route.params.categoryId;
-            //     return { title: catId };
-            //   }
-            // }
-          />
-          <Stack.Screen name="MealDetailScreen" component={MealDetailScreen} />
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={AppBottomTab} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
